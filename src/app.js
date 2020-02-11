574b844f9f8f5d2cb20474458c3a3416c3a9c503
Riley Bathurst <rileybathurst@rileys-mac-pro.home>
2020-02-10 16:40:09 -0800

    super basic functioning

--- src/app.js
+++ src/app.js
@@ -1,1 +1,15 @@
 import _ from 'lodash';
+
+function menuSwap() {
+    if (menu_small.className === 'active'){
+        menu_small.className = 'inactive';
+    } else {
+        menu_small.className = 'active';
+    }
+}
+
+const btn = document.createElement('button');
+btn.innerHTML = 'Menu';
+btn.onclick = menuSwap;  // onclick event is bind to the original printMe function
+
+document.getElementById('header_menu').appendChild(btn);
