b021dc820afcac53045a7911f701b9cf152178ad
Riley Bathurst <rileybathurst@rileys-mac-pro.home>
2020-02-16 13:22:45 -0800

    menu button ripple is working but needs cleaning up

--- app.js
+++ app.js
@@ -1,43 +1,63 @@
-// import _ from 'lodash';
+const btn = document.createElement('button');
+btn.innerHTML = 'Menu'
 
-function menuSwap() {
+document.getElementById('header_menu').appendChild(btn);
+
+// start with an interior span
+const spa = document.createElement('span');
+spa.className = 'ripple';
+
+// grab the button width
+var rect = btn.getBoundingClientRect();
+
+// make a square the big size which is width as button should only be one line I could do a greater than thing incase if really needed
+spa.style.height = spa.style.width = Math.max(rect.width, rect.height) + 'px';
+
+// drop it in
+btn.appendChild(spa);
+
+
+
+
+
+
+function show() {
     if (menu_small.className === 'active'){
         menu_small.className = 'inactive';
     } else {
         menu_small.className = 'active';
     }
+
+    spa.classList.remove('show');
+
+    // const ww = document.createElement('p');
+    var xacross = event.pageX;
+    var ydown = event.pageY;
+    // console.log('ydwon = ' + ydown);
+
+
+    // spa.style.top = '20px';
+    // spa.style.top = ydown + 'px';
+    // spa.style.left = xacross + 'px';
+
+    // how far down the button is on the page
+    console.log(rect.top);
+
+    // height of the span including padding but not margin
+    console.log(spa.offsetHeight);
+
+    // top = click point on y minus how far down the button is minus the button height divided by 2 - minus how far the page has been scrolled
+    // var top = ydown - rect.top - spa.offsetHeight / 2 - document.body.scrollTop; // just copying the math at this point
+    var top = ((ydown - rect.top) - (spa.offsetHeight / 2)) - document.body.scrollTop; // just copying the math at this point
+    // has a problem if the page is loaded further down
+    console.log(top);
+    spa.style.top = top + 'px';
+
+    // var left = ((xacross - rect.left) - (spa.offsetWidth / 2)) - document.body.scrollLeft;
+    var left = ((xacross - rect.left) - (spa.offsetWidth / 2)) - document.body.scrollLeft; // this might just be working because its the furthest left element
+    spa.style.left = left + 'px';
+
+    spa.classList.add('show');
 }
 
-const btn = document.createElement('button');
-btn.className = 'mdc-button mdc-button--outlined';  // onclick event is bind to the original printMe function
-btn.onclick = menuSwap;
-
-const rip = document.createElement('div');
-rip.className = 'mdc-button__ripple';
-
-const spa = document.createElement('span');
-spa.className = 'mdc-button__label';
-spa.innerHTML = 'Menu';
-
-btn.appendChild(rip);
-btn.appendChild(spa);
-
-document.getElementById('header_menu').appendChild(btn);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-import {MDCRipple} from '@material/ripple/index';
-new MDCRipple(document.querySelectorAll('.mdc-button')); // I think this needs to be a query all
+btn.onclick = show;
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
-const path = require('path');
-const glob = require('glob');
+const autoprefixer = require('autoprefixer');
 
-module.exports = [{
+module.exports = {
   entry: ['./app.scss', './app.js'],
   output: {
     filename: 'bundle.js',
@@ -22,31 +21,26 @@ module.exports = [{
           {
             loader: 'postcss-loader',
             options: {
-              plugins: () => [autoprefixer()]
+               plugins: () => [autoprefixer()]
             }
           },
-          { loader: 'sass-loader',
-            options: {
-              sourceMap: true,
-
-              // mdc-web doesn't use sass-loader's normal syntax for imports
-              // across modules, so we add all module directories containing
-              // mdc-web components to the Sass include path
-              // https://github.com/material-components/material-components-web/issues/351
-              includePaths: glob.sync(
-                path.join(__dirname, '**/node_modules/@material')
-              ).map((dir) => path.dirname(dir)),
-            },
+          {
+            loader: 'sass-loader'
           },
         ]
       },
       {
         test: /\.js$/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['@babel/preset-env'],
-        },
-      }
-    ]
-  },
-}];
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env'
+            ]
+          }
+        }
+      },
+    ],
+  }
+};
