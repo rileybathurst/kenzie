fe85823c5e8c9a6dcd4f16c94a10e1448bd19dc8
Riley <riley@rileybathurst.com>
2022-08-01 08:53:33 -0700

    working something

--- .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -4,7 +4,6 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": preferDefault(require("/Users/rileybathurst/Sites/kenzie/.cache/dev-404-page.js")),
   "component---src-pages-404-tsx": preferDefault(require("/Users/rileybathurst/Sites/kenzie/src/pages/404.tsx")),
   "component---src-pages-index-tsx": preferDefault(require("/Users/rileybathurst/Sites/kenzie/src/pages/index.tsx"))
 }
