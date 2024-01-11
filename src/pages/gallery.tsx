a92314e8a81712e141b95ab2cd10465ef4344bf4
Riley <riley@rileybathurst.com>
2024-01-10 19:49:40 -0800

    seo import update

--- src/pages/gallery.tsx
+++ src/pages/gallery.tsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 import Header from "../components/header"
 import Footer from "../components/footer"
-
+import { SEO } from "../components/seo"
 import Carousel from "../components/carousel"
 
 const PhotoPage = () => {
